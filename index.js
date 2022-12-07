b478af9380f418bb96946754e8675052d0a112be
João Santos <118611278+vilelas@users.noreply.github.com>
2022-12-07 13:01:39 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const usuariosRoute = require("./routes/usuario")
+const produtosRoute = require("./routes/produto");
 
 mongoose
   .connect(process.env.MONGO_URL)
@@ -20,6 +22,7 @@ app.use(express.json());
 // Rotas 
 app.use("/api/auth", authRoute);
 app.use("/api/usuarios", usuariosRoute);
+app.use("/api/produtos", produtosRoute);
 
 app.listen(5000, () => {
   console.log("O servidor está rodando!");
